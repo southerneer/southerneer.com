817d4df4e4689bb6dedba15ef7f483f9891d350f
Eric Kirkham <eric.kirkham@gmail.com>
2014-11-04 15:27:32 -0500

    cleaning up

--- wordpress_export_migrate.sh
+++
@@ -1,6 +0,0 @@
-ruby -rubygems -e 'require "jekyll-import";
-    JekyllImport::Importers::WordpressDotCom.run({
-      "source" => "/Users/kirkham/Downloads/southerneer.wordpress.2014-11-04.xml",
-      "no_fetch_images" => false,
-      "assets_folder" => "assets"
-    })'
