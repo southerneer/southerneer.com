46f9f846af81b5f71ae844e18d05e0f6757f46cc
Eric Kirkham <eric@southerneer.com>
2015-04-06 08:34:45 -0400

    removed the form and rotating pics on the homepage

--- app/_assets/scripts/application.js
+++ app/_assets/scripts/application.js
@@ -20,36 +20,3 @@ ga('send', 'pageview');
 $(function() {
   $('.vidContainer').fitVids();
 });
-
-
-
-
-$('form.contact-form').on('submit', function() {
-  $('.form-error').remove();
-
-  var validated = true;
-
-  //validation
-  $(this).find("input[type=text], input[type=email], textarea").each( function(index) {
-    if($(this).val() == '') {
-      $('form').prepend('<p class="form-error">Please fill in all fields. ;)</p>');
-      validated = false;
-      return false;
-    }
-  });
-
-  if( !validated ) return false;
-
-  $.ajax({
-      data: $(this).serialize(),
-      dataType: 'jsonp',
-      type: $(this).attr('method'),
-      url: 'https://getsimpleform.com/messages/ajax?form_api_token=0fbbe2615b633f2c158f18c39812ca3e'
-  })
-  .done(function() {
-    $('form.contact-form').replaceWith('<h2 style="text-align:center; margin-top:1em">Thanks for contacting us. We will get back to you shortly.</h2>');
-    $('form.contact-form').find("input[type=text], input[type=email], textarea").val("");
-  });
-
-  return false;
-});
