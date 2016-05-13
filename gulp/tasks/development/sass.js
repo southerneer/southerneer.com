7dc9c637dac6f13aed3e4c66f6e1e1dae73a7913
Eric Kirkham <eric@southerneer.com>
2016-05-13 16:37:28 -0700

    got the sass stuff all figured out

--- gulp/tasks/development/sass.js
+++ gulp/tasks/development/sass.js
@@ -1,43 +1,46 @@
 var gulp         = require('gulp');
 var plumber      = require('gulp-plumber');
 var browsersync  = require('browser-sync');
-var sass         = require('gulp-ruby-sass');
+var sass         = require('gulp-sass');
 var gulpFilter   = require('gulp-filter');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps   = require('gulp-sourcemaps');
 var config       = require('../../config');
+var debug        = require('gulp-debug');
 
-/**
- * Generate CSS from SCSS
- * Build sourcemaps
- */
 gulp.task('sass', function() {
-  var sassConfig = config.sass.options;
+  return gulp.src(config.sass.src)
+    // .pipe(debug())
+    // .pipe(plumber())
 
-  sassConfig.onError = browsersync.notify;
-
-  // Don’t write sourcemaps of sourcemaps
-  var filter = gulpFilter(['*.css', '!*.map']);
-
-  browsersync.notify('Compiling Sass');
-
-  // return gulp.src(config.sass.src)
-  return sass(config.sass.src, sassConfig)
-    .on('error', sass.logError)
     // .pipe(sourcemaps.init())
-    // .pipe(autoprefixer(config.autoprefixer))
-    // .pipe(filter) // Don’t write sourcemaps of sourcemaps
-    // .pipe(sourcemaps.write('.', { includeContent: false }))
-    // .pipe(filter.restore()) // Restore original files
+    // .pipe(sass.sync({
+    //   outputStyle: 'expanded',
+    //   precision: 10,
+    //   includePaths: ['.']
+    // }).on('error', sass.logError))
+    // // .pipe(autoprefixer({browsers: ['last 1 version']}))
+    // .pipe(sourcemaps.write())
 
-    // for inline sourcemaps
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    
-    // for file sourcemaps
-    // .pipe(sourcemaps.write('maps', {
-    //     includeContent: false,
-    //     sourceRoot: 'source'
-    // }))
 
-    .pipe(gulp.dest(config.sass.dest));
+
+    .pipe(gulp.dest(config.sass.dest))
+    // .pipe(reload({stream: true}));
 });
+
+// gulp.task('sass', function () {
+//   return gulp.src('./sass/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./css'));
+// });
+
+// gulp.task('sass', function () {
+//  return gulp.src('./sass/**/*.scss')
+//   .pipe(sourcemaps.init())
+//   .pipe(sass().on('error', sass.logError))
+//   .pipe(sourcemaps.write())
+//   .pipe(gulp.dest('./css'));
+// });
